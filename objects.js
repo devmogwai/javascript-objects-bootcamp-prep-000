f214daf5440875a65814e6626c9f7780513e21fa
Daniel Doughty <djdoughty@gmail.com>
2018-04-03 20:26:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {'sublime':'what i got'}
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle
+  playlist[artistName] = songTitle
   console.log(playlist)
   }
   updatePlaylist(playlist, "311", "Love Song")
